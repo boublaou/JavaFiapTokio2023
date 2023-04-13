48cffe38ddf16e419977405b519c5276dacfc424
boublaou <ikramboublaou@gmail.com>
2023-04-13 18:25:12 -0300

    primeiro commit java logica

---
+++ JavaTokio2023/src/logico/out/SegundoPrograma.java
@@ -0,0 +1,28 @@
+package logico.out;
+
+import java.util.Scanner;
+
+public class SegundoPrograma {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("digite seu nome: ");
+		String nome = entrada.nextLine();
+		
+		
+		System.out.println("Digite sua idade: ");
+		System.out.println("Sua idade e: " + entrada.nextLine());
+		
+		System.out.println("Bem-Vindo, " + nome);
+		
+	
+	
+	
+	
+	
+	
+	}
+	
+}
